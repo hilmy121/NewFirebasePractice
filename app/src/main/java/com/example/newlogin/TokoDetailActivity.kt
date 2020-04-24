ff26d54c4aa90d5799cf8e8e6a445325cc310d16
Hilmy <caklunim@gmail.com>
2020-04-24 20:52:16 +0700

    lelah

---
+++ app/src/main/java/com/example/newlogin/TokoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TokoDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toko_detail)
+    }
+}
